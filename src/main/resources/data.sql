4f610388a59a6e6c508a947754ca11c6a633e284
hibiki.suzuki <hbear1029@outlook.jp>
2024-09-17 00:02:12 +0900

    Lesson18Chapter 4コミット

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO user(name, gender, age, email) VALUE("キラメキ太郎", "男性", 27, 'taro.kirameki@mail.com');
+INSERT INTO user(name, gender, age, email) VALUE("キラメキ次郎", "男性", 22, 'jiro.kirameki@mail.com');
+INSERT INTO user(name, gender, age, email) VALUE("キラメキ花子", "女性", 25, 'hanako.kirameki@mail.com');
